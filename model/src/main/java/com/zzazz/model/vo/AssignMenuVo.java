471cd763eaf62933a8ba481eb8a1cf027b4fa5b6
nan_kokuchu <nan_k@cloudseed.com>
2024-05-17 16:24:31 +0900

    フロントで送るデータを貰うBean

---
+++ model/src/main/java/com/zzazz/model/vo/AssignMenuVo.java
@@ -0,0 +1,26 @@
+package com.zzazz.model.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * ClassName: AssignMenuVo
+ * Package: com.zzazz.model.vo
+ *
+ * @Author: zzazz
+ * @Create: 2024/5/17 - 15:23
+ * @Description: AssignMenuVo
+ * @Version: v1.0
+ */
+@ApiModel(description = "メニュー割り当て")
+@Data
+public class AssignMenuVo {
+    @ApiModelProperty(value = "ロールID")
+    private Long roleId;
+
+    @ApiModelProperty(value = "メニューIDリスト")
+    private List<Long> menuIdList;
+}
