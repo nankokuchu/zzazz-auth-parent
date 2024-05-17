3bdeedc3ca7739b1dd6a2eae0f71afdcd7a5c232
nan_kokuchu <nan_k@cloudseed.com>
2024-05-17 12:20:26 +0900

    CRUDを実装

--- service-system/src/main/java/com/zzazz/system/controller/SysMenuController.java
+++ service-system/src/main/java/com/zzazz/system/controller/SysMenuController.java
@@ -1,8 +1,14 @@
 package com.zzazz.system.controller;
 
+import com.zzazz.common.result.R;
+import com.zzazz.model.system.SysMenu;
+import com.zzazz.system.service.SysMenuService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * ClassName: SysMenuController
@@ -18,4 +24,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin/system/sysMenu")
 public class SysMenuController {
 
+    @Autowired
+    private SysMenuService sysMenuService;
+
+    // R->List
+    @ApiOperation(value = "全てのメニューをTREEの形式で取得")
+    @GetMapping("/getMenu")
+    public R getMenu() {
+        List<SysMenu> list = sysMenuService.getMenuByNodes();
+        return R.ok(list);
+    }
+
+    // C
+    @ApiOperation(value = "メニューを保存")
+    @PostMapping("/saveMenu")
+    public R<Void> saveMenu(@RequestBody SysMenu sysMenu) {
+        boolean save = sysMenuService.save(sysMenu);
+        if (save) {
+            return R.ok();
+        }
+        return R.fail();
+    }
+
+    // U
+    @ApiOperation(value = "メニューを更新")
+    @PutMapping("/updateMenu")
+    public R<Void> updateMenu(@RequestBody SysMenu sysMenu) {
+        boolean isUpdate = sysMenuService.updateById(sysMenu);
+        if (isUpdate) {
+            return R.ok();
+        }
+        return R.fail();
+    }
+
+    // D
+    @ApiOperation(value = "メニューを削除")
+    @DeleteMapping("/removeMenu/{id}")
+    public R<Void> removeMenu(@PathVariable Long id) {
+        boolean isRemove = sysMenuService.removeById(id);
+        if (isRemove) {
+            return R.ok();
+        }
+        return R.fail();
+    }
 }
