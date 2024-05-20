d705bcee74b60946e22b0350c8568349e002f47e
nan_kokuchu <nan_k@cloudseed.com>
2024-05-20 10:31:01 +0900

    LoginVoのBean

---
+++ model/src/main/java/com/zzazz/model/vo/LoginVo.java
@@ -0,0 +1,33 @@
+package com.zzazz.model.vo;
+
+/**
+ * ClassName: LoginVo
+ * Package: com.zzazz.model.vo
+ *
+ * @Author: zzazz
+ * @Create: 2024/5/20 - 9:52
+ * @Description: LoginVo
+ * @Version: v1.0
+ */
+public class LoginVo {
+
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
