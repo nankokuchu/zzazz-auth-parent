c3a3392bdb17e7d769b728213e0c5bc8ee82c2e7
nan_kokuchu <nan_k@cloudseed.com>
2024-05-20 17:03:18 +0900

    Spring SecurityのUserDetailsをカスタマイズする。

---
+++ common/spring-security/src/main/java/com/zzazz/custom/CustomUser.java
@@ -0,0 +1,33 @@
+package com.zzazz.custom;
+
+import com.zzazz.model.system.SysUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+/**
+ * ClassName: CustomUser
+ * Package: com.zzazz.custom
+ *
+ * @Author: zzazz
+ * @Create: 2024/5/20 - 16:49
+ * @Description: CustomUser; SpringSecurityのUserDetailsの拡張
+ * @Version: v1.0
+ */
+public class CustomUser extends User {
+    private SysUser sysUser;
+
+    public CustomUser(SysUser sysUser, Collection<? extends GrantedAuthority> authorities) {
+        super(sysUser.getUsername(), sysUser.getPassword(), authorities);
+        this.sysUser = sysUser;
+    }
+
+    public SysUser getSysUser() {
+        return sysUser;
+    }
+
+    public void setSysUser(SysUser sysUser) {
+        this.sysUser = sysUser;
+    }
+}
