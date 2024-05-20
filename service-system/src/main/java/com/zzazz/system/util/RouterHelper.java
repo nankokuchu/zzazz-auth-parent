2ca3d6bd7df1eea36acacb4f5c875c93b629554b
nan_kokuchu <nan_k@cloudseed.com>
2024-05-20 11:59:29 +0900

    メニューからrouteを構築

---
+++ service-system/src/main/java/com/zzazz/system/util/RouterHelper.java
@@ -0,0 +1,77 @@
+package com.zzazz.system.util;
+
+import com.zzazz.model.system.SysMenu;
+import com.zzazz.model.vo.MetaVo;
+import com.zzazz.model.vo.RouterVo;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * ClassName: RouterHelper
+ * Package: com.zzazz.system.util
+ *
+ * @Author: zzazz
+ * @Create: 2024/5/20 - 11:47
+ * @Description: RouterHelper
+ * @Version: v1.0
+ */
+public class RouterHelper {
+    /**
+     * メニューからrouteを構築
+     *
+     * @param menus Tree形式のSysMenuリスト
+     * @return List<RouterVo>
+     */
+    public static List<RouterVo> buildRouters(List<SysMenu> menus) {
+        List<RouterVo> routers = new LinkedList<>();
+        for (SysMenu menu : menus) {
+            RouterVo router = new RouterVo();
+            router.setHidden(false);
+            router.setAlwaysShow(false);
+            router.setPath(getRouterPath(menu));
+            router.setComponent(menu.getComponent());
+            router.setMeta(new MetaVo(menu.getName(), menu.getIcon()));
+            List<SysMenu> children = menu.getChildren();
+            //如果当前是菜单，需将按钮对应的路由加载出来，如：“角色授权”按钮对应的路由在“系统管理”下面
+            if (menu.getType() == 1) {
+                List<SysMenu> hiddenMenuList = children.stream().filter(item -> !StringUtils.isEmpty(item.getComponent())).collect(Collectors.toList());
+                for (SysMenu hiddenMenu : hiddenMenuList) {
+                    RouterVo hiddenRouter = new RouterVo();
+                    hiddenRouter.setHidden(true);
+                    hiddenRouter.setAlwaysShow(false);
+                    hiddenRouter.setPath(getRouterPath(hiddenMenu));
+                    hiddenRouter.setComponent(hiddenMenu.getComponent());
+                    hiddenRouter.setMeta(new MetaVo(hiddenMenu.getName(), hiddenMenu.getIcon()));
+                    routers.add(hiddenRouter);
+                }
+            } else {
+                if (!CollectionUtils.isEmpty(children)) {
+                    if (children.size() > 0) {
+                        router.setAlwaysShow(true);
+                    }
+                    router.setChildren(buildRouters(children));
+                }
+            }
+            routers.add(router);
+        }
+        return routers;
+    }
+
+    /**
+     * routeのpathを取得
+     *
+     * @param menu SysMenu
+     * @return routeのpath
+     */
+    public static String getRouterPath(SysMenu menu) {
+        String routerPath = "/" + menu.getPath();
+        if (menu.getParentId().intValue() != 0) {
+            routerPath = menu.getPath();
+        }
+        return routerPath;
+    }
+}
