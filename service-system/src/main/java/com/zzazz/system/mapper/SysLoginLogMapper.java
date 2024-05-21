85e90e569a40a83e2b943aade89eee0b984f826a
nan_kokuchu <nan_k@cloudseed.com>
2024-05-21 12:56:30 +0900

    登録ログのmapper

---
+++ service-system/src/main/java/com/zzazz/system/mapper/SysLoginLogMapper.java
@@ -0,0 +1,18 @@
+package com.zzazz.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zzazz.model.system.SysLoginLog;
+import org.springframework.stereotype.Repository;
+
+/**
+ * ClassName: SysLoginLogMapper
+ * Package: com.zzazz.system.mapper
+ *
+ * @Author: zzazz
+ * @Create: 2024/5/21 - 12:36
+ * @Description: SysLoginLogMapper
+ * @Version: v1.0
+ */
+@Repository
+public interface SysLoginLogMapper extends BaseMapper<SysLoginLog> {
+}
