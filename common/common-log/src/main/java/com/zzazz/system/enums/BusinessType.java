159e10f8c1ad2817a93b640dac8b6c3962f2349c
nan_kokuchu <nan_k@cloudseed.com>
2024-05-21 15:35:53 +0900

    enumを追加

---
+++ common/common-log/src/main/java/com/zzazz/system/enums/BusinessType.java
@@ -0,0 +1,62 @@
+package com.zzazz.system.enums;
+
+/**
+ * ClassName: BusinessType
+ * Package: com.zzazz.system.enums
+ *
+ * @Author: zzazz
+ * @Create: 2024/5/21 - 14:00
+ * @Description: BusinessType
+ * @Version: v1.0
+ */
+public enum BusinessType {
+    /**
+     * 其它
+     */
+    OTHER,
+
+    /**
+     * 新增
+     */
+    INSERT,
+
+    /**
+     * 修改
+     */
+    UPDATE,
+
+    /**
+     * 删除
+     */
+    DELETE,
+
+    /**
+     * 授权
+     */
+    ASSGIN,
+
+    /**
+     * 导出
+     */
+    EXPORT,
+
+    /**
+     * 导入
+     */
+    IMPORT,
+
+    /**
+     * 强退
+     */
+    FORCE,
+
+    /**
+     * 更新状态
+     */
+    STATUS,
+
+    /**
+     * 清空数据
+     */
+    CLEAN,
+}
---
+++ common/common-log/src/main/java/com/zzazz/system/enums/OperatorType.java
@@ -0,0 +1,27 @@
+package com.zzazz.system.enums;
+
+/**
+ * ClassName: OperatorType
+ * Package: com.zzazz.system.enums
+ *
+ * @Author: zzazz
+ * @Create: 2024/5/21 - 13:59
+ * @Description: OperatorType
+ * @Version: v1.0
+ */
+public enum OperatorType {
+    /**
+     * その他
+     */
+    OTHER,
+
+    /**
+     * 管理者
+     */
+    MANAGE,
+
+    /**
+     * モバイルユーザー
+     */
+    MOBILE
+}
