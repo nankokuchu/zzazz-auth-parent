5a57c463d13341e57c64f94f464777d39bbdb792
nan_kokuchu <nan_k@cloudseed.com>
2024-05-22 12:04:56 +0900

    SpringSecurityの例外処理を実装

--- common/service-util/src/main/java/com/zzazz/system/handler/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/zzazz/system/handler/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.zzazz.system.handler;
 
 import com.zzazz.common.result.R;
+import com.zzazz.common.result.ResultCodeEnum;
 import com.zzazz.system.exception.ZzazzException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,21 +22,28 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public R error(Exception e){
+    public R error(Exception e) {
         e.printStackTrace();
         return R.fail().message("異常が発生しました。");
     }
 
     @ExceptionHandler(ArithmeticException.class)
     @ResponseBody
-    public R error(ArithmeticException e){
+    public R error(ArithmeticException e) {
         e.printStackTrace();
         return R.fail().message("特定の異常が発生しました");
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseBody
+    public R error(AccessDeniedException e) throws AccessDeniedException {
+        e.printStackTrace();
+        return R.build(null, ResultCodeEnum.PERMISSION);
+    }
+
     @ExceptionHandler({ZzazzException.class})
     @ResponseBody
-    public R error(ZzazzException e){
+    public R error(ZzazzException e) {
         e.printStackTrace();
         return R.fail().message(e.getMessage());
     }
