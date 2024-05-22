c0800ba6242327e8b0c6b08dac7186ec1db3a787
nan_kokuchu <nan_k@cloudseed.com>
2024-05-22 12:08:01 +0900

    権限付与

--- common/spring-security/src/main/java/com/zzazz/system/fillter/TokenAuthenticationFilter.java
+++ common/spring-security/src/main/java/com/zzazz/system/fillter/TokenAuthenticationFilter.java
@@ -4,7 +4,11 @@ import com.zzazz.common.result.R;
 import com.zzazz.common.result.ResultCodeEnum;
 import com.zzazz.common.util.JwtHelper;
 import com.zzazz.common.util.ResponseUtil;
+import com.zzazz.system.service.SysAuthMenuService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,7 +18,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * ClassName: TokenAuthenticationFilter
@@ -26,9 +32,13 @@ import java.util.Collections;
  * @Version: v1.0
  */
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
-    public TokenAuthenticationFilter() {
+    private static final Logger log = LoggerFactory.getLogger(TokenAuthenticationFilter.class);
 
+    private final SysAuthMenuService sysAuthMenuService;
+    public TokenAuthenticationFilter(SysAuthMenuService sysAuthMenuService) {
+        this.sysAuthMenuService = sysAuthMenuService;
     }
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
@@ -55,9 +65,23 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         // logger.info("token:"+token);
         if (!StringUtils.isEmpty(token)) {
             String username = JwtHelper.getUsername(token);
+            Long userId = JwtHelper.getUserId(token);
             // logger.info("username:"+username);
             if (!StringUtils.isEmpty(username)) {
-                return new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+                // ボタン権限を取得
+                List<String> userButtonList = sysAuthMenuService.getUserButtonList(userId);
+                // log.info(String.valueOf(userButtonList));
+                // redisの代わりに、毎回DBから検索するように
+                if (!userButtonList.isEmpty()){
+                    // List<SimpleGrantedAuthority> authorities = new ArrayList<>(); SpringSecurityの形式
+                    List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                    for (String s : userButtonList) {
+                        authorities.add(new SimpleGrantedAuthority(s.trim()));
+                    }
+                    return new UsernamePasswordAuthenticationToken(username, null, authorities);
+                } else {
+                    return new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+                }
             }
         }
         return null;
